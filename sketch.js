d07e353d15f1d98270bf625fe466aab1d45ef016
xushan <xushan.xushan@gmail.com>
2019-11-27 12:49:47 +0100

    update2

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-
+let bg;
 var img
 function preload(){
    sound = loadSound('assets/TG1_bumper.mp3');
    img = loadImage('assets/speaker.png');
+   bg = loadImage('assets/jeremy-thomas-E0AHdsENmDg-unsplash.jpg');
 }
 
 function setup(){
@@ -10,16 +11,14 @@ function setup(){
 
   //press to play
   canvas.mouseClicked(play);
-
   analyzer = new p5.Amplitude();
-
-
   fft = new p5.FFT(0.95,256);
   sound.amp(0.5);
+
 }
 
 function draw(){
-  background('blue');
+  background('navy');
   volume = analyzer.getLevel();
   volume1 = map(volume, 0.5, 1.5, 0.5, height);
   imageMode(CENTER);
@@ -31,7 +30,7 @@ function draw(){
   for (var i = 0; i< spectrum.length; i++){
     var x = map(i, 3, spectrum.length, 0, width*3);
     var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length-5, h/2 )
+    rect(x, height, width / spectrum.length-2, h/2 )
   }
 
 
